6acd19cf9c8d4c466d5cc5b29c5e5087f8cd7096
Rashmitha Piyushan <rashmithapiyushansl@gmail.com>
2023-07-01 18:18:53 +0530

    set up the project

---
+++ manage-servers/src/test/java/lk/ijse/dep10/manageservers/ManageServersApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.manageservers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageServersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
